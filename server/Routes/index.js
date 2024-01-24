6ac3356adb65984e06ca4422aeaf5cd05ce3ef98
tushar-454 <imtushar454@gmail.com>
2024-01-24 08:06:56 +0600

    server jwt create and remove api created

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
+const jwtRoutes = require('./jwt');
 
 router.use('/api/v1/user', userRoutes);
+router.use('/api/v1/jwt', jwtRoutes);
 
 module.exports = router;
